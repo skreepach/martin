8ce077d7d0b7a41be94b9bc7a26ce62cac9becfb
skreepach <35807335+skreepach@users.noreply.github.com>
2018-05-18 14:07:28 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=fdb;host='heroku_35a63eb77fee245','root','77c43c35');
+\atk4\data\Persistence_SQL('mysql:dbname=fdb;host='heroku_35a63eb77fee245','bfa11752c77b7d','77c43c35');
 
 class Friends extends \atk4\data\Model {
   public $table = 'friends';
