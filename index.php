2d846021005deec4a2a56469638202786c435bda
skreepach <35807335+skreepach@users.noreply.github.com>
2018-05-18 14:45:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require 'vendor/autoload.php';
 require 'connection.php';
 $app = new \atk4\ui\App('Martin');
 $layout = $app->initLayout('Centered');
-require 'visual.php';
 $form = $app->add('Form');
 $form->setModel(new Friends($db));
 ?>
