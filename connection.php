4a60d6a5427c30d9a239a706499ca77bdc89b474
skreepach <35807335+skreepach@users.noreply.github.com>
2018-05-18 14:16:38 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ $db = new
 \atk4\data\Persistence_SQL('mysql:dbname=fdb;host='heroku_35a63eb77fee245','bfa11752c77b7d','77c43c35');
 
 class Friends extends \atk4\data\Model {
-  public $table = 'friends';
+  public $table = 'martin';
   function init() {
     parent::init();
     $this->addField('nickname');
