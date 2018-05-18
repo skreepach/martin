ce354f7b65699ca9adede4517228b13ee5b22b53
skreepach <35807335+skreepach@users.noreply.github.com>
2018-05-18 14:35:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require 'connection.php';
 $app = new \atk4\ui\App('Martin');
 require 'visual.php';
 $layout = $app->initLayout('Centered');
-$form = $app->add('Form');
+/*$form = $app->add('Form');
 $form->setModel(new Friends($db));
-?> 
+?> */
